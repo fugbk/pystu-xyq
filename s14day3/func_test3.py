9053c30a790cb2e2d0fa45fae00963ae038d4fe1
liAng <ali6102@163.com>
2019-06-19 18:02:06 +0800

    默认更改列表

--- s14day3/func_test3.py
+++ s14day3/func_test3.py
@@ -2,7 +2,24 @@
 # encoding = utf-8
 # Email : ali6102@163.com
 
-def test(*args):
-    print(args)
+# def test(*args):
+#     print(args)
+#
+# test(1,2,3,24,3)
 
-test(1,2,3,24,3)
+
+
+
+
+def change_name():
+    name = ['A','B','C','D']
+    name[0] = "X"
+    print("in func",name)
+change_name()
+
+
+
+def calc(n):
+    print(n)
+    return calc(n+1)
+calc(1)
