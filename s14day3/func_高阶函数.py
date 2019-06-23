8edac705a8f6273da87644e90e0a434cb4609610
liAng6102 <51697582+liAng6102@users.noreply.github.com>
2019-06-23 18:06:53 +0800

    fsfdsaf

--- s14day3/func_高阶函数.py
+++ s14day3/func_高阶函数.py
@@ -2,10 +2,43 @@
 # encoding = utf-8
 # Email : ali6102@163.com
 
+def cut_off():
+    cut_off_flog = "*"
+    print(cut_off_flog.ljust(70,'*'))
+cut_off()
+
 
 def add(a,b,f):
     return f(a) + f(b)
 c = add(-3,-4,abs)
 
 print(c)
+print(cut_off)
+#-------------------------------------------------------------------------
 
+import time
+def test():
+    time.sleep(1)
+    print("in the test")
+
+def bar(func):
+    start_time = time.time()
+    stop_time = time.time()
+    print("the func run time is %s" %(stop_time - start_time))
+    return func
+
+test = bar(test)
+test()
+print(cut_off)
+#---------------------------------------------------------------------------
+
+import time
+def test2():
+    print("in the test2")
+
+def foo(func):
+    print(time.time())
+    return func
+
+test2 = foo(test2)
+test2()
--- s14day3/装饰器/1.1 装饰器.py
+++ s14day3/装饰器/1.1 装饰器.py
@@ -1,4 +1,17 @@
 # Author: LiAng
 # encoding = utf-8
 # Email : ali6102@163.com
+import time
+def deco(func): # <==> deco(test2) ------> func=test2
+    def timer(*args,**kwargs): # ==> (*args,**kwargs) == ('ali')
+        start_time = time.time()
+        func(*args,**kwargs) # <==> test2('ali')
+        stop_time = time.time()
+        print("func run time is %s" %(stop_time - start_time))
+    return timer
+@deco  # <==> test2 = deco(test2(name))  name=str(ali)
+def test2(name):
+    time.sleep(1)
+    print("in the test2 , I'm %s " %name)
 
+test2('ali')
