14aa688cdf3e47c5088354e542b48c8069cde10b
liAng <ali6102@163.com>
2019-07-24 17:36:07 +0800

    重新提交

---
+++ 练习/备份程序.py
@@ -0,0 +1,15 @@
+# encoding = utf-8
+__author__ = "Ang Li"
+
+
+def cut_off():
+    cut_off_flog = "*"
+    print(cut_off_flog.ljust(70, '*'))
+
+
+cut_off()
+
+cut_off()
+
+
+print('aaaaaaaaaaaaaaaaaaaaaaaa')
