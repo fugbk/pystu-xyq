caf98af19836ac518a7bd50fe8b36ad64c33deec
liAng <ali6102@163.com>
2019-08-22 18:05:31 +0800

    面向对象进阶-类方法属性方法

--- 附录/2 面向对象进阶/2.1 类方法.py
+++ 附录/2 面向对象进阶/2.1 类方法.py
@@ -11,12 +11,19 @@ class Dog(object):
         print("eat ---> %s" % self) # eat ---> <class '__main__.Dog'> # 打印的是类本身
         print("Dog %s is eating..." % self.name) # self 先找实例变量,实例变量找不到再找类变量
 
+    @property
     def sleep(self):
-        print("sleep ---> %s" % self) # sleep ---> <__main__.Dog object at 0x00000217D086CDC8> 打印Dog这个对象
+        #print("sleep ---> %s" % self) # sleep ---> <__main__.Dog object at 0x00000217D086CDC8> 打印Dog这个对象
         print("Dog %s is sleeping..." %(self.name))
 
+    @sleep.setter
+    def sleep(self,status):
+        self.status = status
+        print(self.status)
 
 d = Dog("Tom")
-d.eat()
-d.sleep()
+d.sleep
+d.sleep = 1
+
+
 
